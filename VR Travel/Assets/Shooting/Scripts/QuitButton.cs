9a66b9b83f79300b54bed887f8ea6907bd5160a8
Alistair <alistair.p.r.bartlett@gmail.com>
2021-08-25 19:04:56 +0800

    Lighting Builds
    lighting builds

--- VR Travel/Assets/Shooting/Scripts/QuitButton.cs
+++ VR Travel/Assets/Shooting/Scripts/QuitButton.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class QuitButton : MonoBehaviour
 {
@@ -9,7 +10,7 @@ public class QuitButton : MonoBehaviour
         if (other.tag == "Bullet")
         {
             Debug.Log("Ran Quit Button, Load Scene Main Menu");
-            //SceneManager.LoadScene("MainMenu");
+            SceneManager.LoadScene("Main hub");
         }
     }
 }
